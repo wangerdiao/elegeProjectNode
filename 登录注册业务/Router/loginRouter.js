559447cdb776182fdc70c0e40ac0061ad72a1e37
王derful <11409153+wang-derful@user.noreply.gitee.com>
2022-11-04 23:25:34 +0800

    jwt

--- 登录注册业务/Router/loginRouter.js
+++ 登录注册业务/Router/loginRouter.js
@@ -1,18 +1,21 @@
 //专门管理登录业务的路由
 const {Router} = require('express')
+const JWT = require('../util/jwt')
 //创建路由器
 const router = new Router()
 //引入模型对象进行CURD
 const userModel = require('../model/user')
-const sha1 = require('sha1')
+const sha1 = require('sha1') 
 //登录业务
 router.post('/login',(req,res) => {
+    console.log(req.body)
     const {account,password} = req.body
-    res.setHeader('Access-Control-Allow-Origin','*')
+      const token = JWT.generate({data:account},"2s")
+      const decoded = JWT.verify(token)
     //查找用户是否存在
     userModel.findOne({account,password:sha1(password)},(err,data) => {
         if(err) console.log('请求失败')
-        if(data) res.send("code:200")
+        if(data)  res.json({data:200,message:'成功'})
         else res.send('请求失败')
     })
     
--- 登录注册业务/Router/registerRouter.js
+++ 登录注册业务/Router/registerRouter.js
@@ -7,22 +7,19 @@ const userModel = require('../model/user')
 const sha1 = require('sha1') //用于加密数据
 //登录业务
 router.post('/register',(req,res) => {
-    console.log(1)
     const {account,password} = req.body
-    console.log(account,password)
     //查找账户是否存在
     userModel.findOne({account},(err,data) => {
         if(data || err) res.send('请求失败')
         else {
             //在数据库中存储账户和密码
             userModel.create({account,password:sha1(password)},(err,data) => {
-                console.log(3)
-                if(err) console.log('请求失败')
+                if(err) {
+                    res.send('请求失败')
+                    return
+                }
                 if(data) {
-                    console.log(4)
-                    const msg = {code:200,message:'成功'}
-                    console.log(JSON.stringify(msg))
-                    res.json(200,{message:'成功'})
+                    res.json({data:200,message:'成功'})
                 }
                 else res.send('请求失败')
             })
